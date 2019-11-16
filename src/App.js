68173be325d757a56529fb0aaab114b9ba49f1de
Fabián Moreno Islas <fabianislas@apps-fa.com>
2019-11-16 09:47:00 -0600

    make it better

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
+// import { Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import background from './media/img/andromeda-background.jpg';
+import logo from './media/img/andromeda.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <main class="bg-center d-flex align-items-center" style={{backgroundImage: `url(${background})`, height: `${window.innerHeight}px`}}>
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-12 text-center text-white">
+            <img src={logo} width="20%" class="mb-3"/>
+            <h1 class="text-uppercase font-space-age">Andromeda</h1>
+            <h5 class="mb-4">Que la fuerza te acompañe</h5>
+            <button class="btn btn-lg rounded-pill btn-light" type="button">Empezar viaje</button>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
+
+
 
 export default App;
